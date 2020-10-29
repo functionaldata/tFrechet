f2b985e88f439e0e359083176e845596b014717b
Yaqing Chen <yaqchen@ucdavis.edu>
2020-10-28 23:30:54 -0700

    documentation & output format updates of DenFMean

--- R/DenFMean.R
+++ R/DenFMean.R
@@ -3,12 +3,14 @@
 #' @param yin A matrix or list holding the sample of measurements for the observed distributions. If \code{yin} is a matrix, each row holds the measurements for one distribution.
 #' @param hin A list holding the histograms of an observed distribution.
 #' @param qin A matrix or list holding the quantile functions of the response. If \code{qin} is a matrix, each row holds the quantile function of an observed distribution taking values on \code{optns$qSup}.
+#' Note that only one of the three \code{yin}, \code{hin}, and \code{qin} needs to be input.
+#' If more than one of them are specified, \code{yin} overwrites \code{hin}, and \code{hin} overwrites \code{qin}.
 #' @param optns A list of options control parameters specified by \code{list(name=value)}.
 #' @details Available control options are \code{qSup}, \code{nqSup}, \code{lower}, \code{upper}, \code{bwDen}, \code{nRegGrid}, \code{delta}, \code{kernelDen}, \code{infSupport}, \code{outputGrid}. See \code{\link{LocDenReg}} for details.
 #' @return A list containing the following components:
-#' \item{dout}{A matrix or list holding the density of the Fréchet mean. If \code{dout} is a matrix, each row gives a density and the domain grid is given in \code{dSup}. If \code{dout} is a list, each element is a list of two components, \code{x} and \code{y}, giving the domain grid and density function values, respectively.}
+#' \item{dout}{A numeric vector holding the density of the Fréchet mean.}
 #' \item{dSup}{A numeric vector giving the domain grid of \code{dout} when it is a matrix.}
-#' \item{qout}{A matrix holding the quantile function of the Fréchet mean.}
+#' \item{qout}{A numeric vector holding the quantile function of the Fréchet mean.}
 #' \item{qSup}{A numeric vector giving the domain grid of \code{qout}.}
 #' \item{optns}{A list of control options used.}
 #' @examples
@@ -40,9 +42,14 @@ DenFMean <- function(yin=NULL, hin=NULL, qin=NULL, optns=list()) {
     }
   }
   res <- GloDenReg(xin = xin, yin = yin, hin = hin, qin = qin, optns = optns)
+  if (!is.vector(res$qout)) {
+    res$qout <- as.vector(res$qout)
+  }
   if (is.list(res$dout)) {
     res$dSup <- res$dout[[1]]$x
-    res$dout <- matrix(res$dout[[1]]$y, nrow = 1)
+    res$dout <- res$dout[[1]]$y
+  } else if (is.matrix(res$dout)) {
+    res$dout <- as.vector(res$dout)
   }
   #with(res, list(dout = dout, dSup = dSup, qout = c(qout), qSup = qSup, optns = optns))
   class(res) <- "denReg"
