9f6104cf4a89a9d50270aa98f090fc5555594a1f
xsch1996 <xsch1996@gmail.com>
2021-09-23 14:10:51 -0400

    Object data code

---
+++ R/ObjCov.R
@@ -0,0 +1,99 @@
+#' @title Object Covariance
+#' @description Calculating generalized Fréchet integrals of 1D distribution (equipped with Wasserstein distance) 
+#' @param tgrid time grid for the time varying object data and covariance function
+#' @param n numbers of observations
+#' @param I: four dimension array of \code{n} x \code{n} matrix of squared distances between the time point u of the ith process and process and the time point v of the jth object process, 
+#' e.g.: \eqn{I[i,j,u,v] = d^2(X_i(u) X_j(v))}
+#' @param K numbers of principal components 
+#' @param smooth logical indicating if the smoothing is enabled when calculating the eigenvalues and eigenfunctions
+#' @return A list of the following:
+#' \item{C}{estimated object covariance (nonsmooth) on the 2D grid of dimension \code{length(tgrid)} X \code{length(tgrid)}}
+#' \item{sC}{estimated object covariance (smooth) on the 2D grid of dimension \code{length(tgrid)} X \code{length(tgrid)}}
+#' \item{tgrid}{tgrid time grid for the time varying object data and covariance function}
+#' \item{K}{numbers of principal components}
+#' \item{phi}{matrix of smooth eigenfunctions (dimension: \code{length(tgrid)} X \code{K})}
+#' \item{lambda}{vector of eigenvalues of dimension \code{K} }
+#' @examples 
+#' ### functional covariate
+#‘ phi1 <- function(x) -cos(pi*x/10)/sqrt(5)
+#' phi2 <- function(x)  sin(pi*x/10)/sqrt(5)
+#' 
+#' lambdaX <- c(4,2)
+#' # training set
+#' n <- 100
+#' N <- 50
+#' tgrid <- seq(0,10,length.out = N)
+#' 
+#' Xi <- matrix(rnorm(2*n),nrow=n,ncol=2)
+#' CovX <- lambdaX[1] * phi1(tgrid) %*% t(phi1(tgrid)) + lambdaX[2] * phi2(tgrid) %*% t(phi2(tgrid))
+#' SampleX <- lambdaX[1]^(1/2) * Xi[,1] %*% t(phi1(tgrid)) + lambdaX[2]^(1/2) * Xi[,2] %*% t(phi2(tgrid))
+#' 
+#' I <- array(0, c(n,n,N,N))
+#' for (u in 1:N){
+#'   for (v in 1:N){
+#'     temp1 <- SampleX[,u]
+#'    temp2 <- SampleX[,v]
+#'     I[,,u,v] <- outer(temp1, temp2, function(v1,v2){
+#'      (v1 - v2)^2
+#'    })
+#'  }
+#' }
+#' 
+#' result_cov <- ObjCov(tgrid, I, 2)
+#' result_cov$lambda #4 2
+#' 
+#' sC <- result_cov$sC
+#' sum((sC-CovX)^2) / sum(sC^2)
+#' sum((phi1(tgrid)-result_cov$phi[,1])^2)/sum(phi1(tgrid)^2)
+#' @references 
+#' \cite{Dubey, P., & Müller, H. G. (2020). Functional models for time‐varying random objects. Journal of the Royal Statistical Society: Series B (Statistical Methodology), 82(2), 275-327.}
+#' @export
+
+
+# source("GCVLwls2DV2.R")
+# source("GetMinb.R")
+# source("getSmoothCov.R")
+# source("GetEigenAnalysisResults.R")
+
+
+ObjCov <- function(tgrid, I, K, smooth=TRUE){
+  require(fdapace)
+  if(length(dim(I))!=4){
+    stop("I must be a four dimensional array")
+  }
+  n = dim(I)[1]
+  N = length(tgrid)
+  if(dim(I)[2]!=n){
+    stop("length of the first and second arguments of array I are inconsistent")
+  }
+  if(dim(I)[3]!=dim(I)[4]){
+    stop("length of the third and fourth arguments of array I are inconsistent")
+  }
+  if(dim(I)[3]!=N){
+    stop("length of tgrid and the length of third arguments of array I are inconsistent")
+  }
+  t <- length(tgrid)
+  C <- matrix(0, nrow = t, ncol = t)
+  # calculate empirical metric autocovariance matrix C 
+  for(u in 1:(t-1)){
+    for(v in (u+1):t){
+      C[u,v] <-  ((sum(I[,,u,v]) - sum(diag(I[,,u,v]))) - (n-1)*sum(diag(I[,,u,v]))) / (2*n*(n-1))
+      C[v,u] <- C[u,v]
+    } 
+    C[u,u] <- sum(sum(I[,,u,u])) / (2*n*(n-1))
+  }
+  C[t,t] <- sum(sum(I[,,t,t])) / (2*n*(n-1))
+  #obtain smooth covariance
+  if(smooth == TRUE){
+    sC <- getSmoothCov(C, tgrid, "GMeanAndGCV", "gauss",n)
+    #derivat the eigenfunctions and eigenvalues
+    eigResult <- GetEigenAnalysisResults(sC, tgrid, optns = list(maxK = K, verbose = FALSE, FVEthreshold = 0.9999))
+  }else{
+    sC <- getSmoothCov(C, tgrid, "GMeanAndGCV", "gauss",n)
+    #derivat the eigenfunctions and eigenvalues
+    eigResult <- GetEigenAnalysisResults(C, tgrid, optns = list(maxK = K, verbose = FALSE, FVEthreshold = 0.9999))
+  }
+  return(list(C=C, sC = sC, tgrid = tgrid, K = K, phi = eigResult$phi, lambda = eigResult$lambda))
+  }
+
+
---
+++ R/object_wass_integral.R
@@ -0,0 +1,100 @@
+#' @title Generalized Fréchet integrals of 1D distribution 
+#' @description Calculating generalized Fréchet integrals of 1D distribution (equipped with Wasserstein distance) 
+#' @param phi eigenfunction along which we want to project the distribution
+#' @param t_out support of phi
+#' @param Q A length(t_out) X length(Qout) matrix whose jth row corresponds to the quantile function on grid Qout for the jth timepoint.   
+#' @param Qout support of the quantile valued process
+#' @return A list of the following:
+#' \item{f}{quantile corresponding to the frechet integral of Q along phi}
+#' @examples 
+#' ### simulation ###
+#' n <- 100
+#' N <- 50
+#' t_out <- seq(0,1,length.out = N)
+#' #simulation as in the paper Dubey, P., & Müller, H. G. (2020). Functional models for time‐varying random objects. 
+#' # Journal of the Royal Statistical Society: Series B (Statistical Methodology), 82(2), 275-327.}
+#' 
+#' phi1 <- function(t){
+#'   (t^2-0.5)/0.3416
+#' }
+#' phi2 <- function(t){
+#'   sqrt(3)*t
+#' }
+#' phi3 <- function(t){
+#'   (t^3 - 0.3571*t^2 - 0.6*t + 0.1786)/0.0895
+#' }
+#' 
+#' Z <- cbind(rnorm(n)*sqrt(12), rnorm(n), runif(n)*sqrt(72), runif(n)*sqrt(9))
+#' mu_vec <- 1 + Z[,1] %*% t(phi1(t_out)) + Z[,2] %*% t(phi3(t_out))
+#' sigma_vec <- 3 + Z[,3] %*% t(phi2(t_out)) + Z[,4] %*% t(phi3(t_out))
+#' 
+#' # grids of quantile function
+#' Nq <- 40
+#' eps <- 0.00001
+#' Qout <- seq(0+eps,1-eps,length.out=Nq)
+#' 
+#' # I: four dimension array of n x n matrix of squared distances between the time point u of the ith process and 
+#' # process and the time point v of the jth object process, 
+#' # e.g.: I[i,j,u,v] <- d_w^2(X_i(u) X_j(v)).
+#' I <- array(0, dim = c(n,n,N,N))
+#' for(i in 1:n){
+#'   for(j in 1:n){
+#'     for(u in 1:N){
+#'       for(v in 1:N){
+#'         #wasserstein distance between distribution X_i(u) and X_j(v) 
+#'         I[i,j,u,v] <- (mu_vec[i,u] - mu_vec[j,v])^2 + (sigma_vec[i,u] - sigma_vec[j,v])^2
+#'       }
+#'     }
+#'   }
+#' }
+#' 
+#' # check ObjCov work 
+#' Cov_result <- ObjCov(t_out, I, 3)
+#' #Cov_result$lambda #12 6 1.75
+#' 
+#' # calculate Q 
+#' i <- 6 # for the ith subject
+#' Q <- t(sapply(1:N, function(t){
+#'   qnorm(Qout, mean = mu_vec[i,t], sd = sigma_vec[i,t])
+#' }))
+#' 
+#' score_result <- object_wass_integral(Cov_result$phi[,1], t_out, Q, Qout)
+#' score_result$f
+#' @references 
+#' \cite{Dubey, P., & Müller, H. G. (2020). Functional models for time‐varying random objects. Journal of the Royal Statistical Society: Series B (Statistical Methodology), 82(2), 275-327.}
+#' @export
+
+object_wass_integral <- function(phi, t_out, Q, Qout){
+  require(fdapace)
+  require(quadprog)
+  require(pracma)
+  require(frechet)
+  
+  m <- length(Qout)
+  if(m<2){
+    stop("length of Q out too small, Please increase ")
+  }
+  if(length(phi)!=length(t_out)){
+    stop("length of t_out and phi inconsistent")
+  }
+  if(dim(Q)[2]!=length(Qout)){
+    stop("column length of Qout and length Qout inconsistent")
+  }
+  if(dim(Q)[1]!=length(t_out)){
+    stop("row length of Qout and length t_out inconsistent")
+  }
+  g_mini <- rep(0, m)
+  phi_out <- phi / fdapace::trapzRcpp(t_out, phi)
+  #g_mini = \int Q(S(t))(u)phi(t)dt
+  for (i in 1:m){
+    g_mini[i] <- fdapace::trapzRcpp(t_out, phi_out * Q[,i])
+  }
+  D <- pracma::Toeplitz(a=c(-1,rep(0,m-2)),
+                       b=c(-1,1, rep(0,m-2)))
+  f <- quadprog::solve.QP(diag(m),g_mini,t(D),rep(0,m-1))
+  return(list(f=f$solution))
+}
+
+
+
+
