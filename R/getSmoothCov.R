fb783836d0f16fcb91c4d14df57fab78d9a44d7c
xsch1996 <xsch1996@gmail.com>
2021-09-23 14:13:24 -0400

    fdapace dependencies

---
+++ R/getSmoothCov.R
@@ -0,0 +1,30 @@
+library(pracma)
+getSmoothCov <- function(C, tgrid, method, kern, n){
+  t = length(tgrid)
+  t1 = tgrid
+  cyy = t(as.vector(C))
+  cxxn = cyy
+  xygrid = pracma::meshgrid(t1)
+  xx = xygrid$X
+  yy = xygrid$Y
+  tpairn = cbind(as.vector(xx), as.vector(yy))
+  win = pracma::ones(1, length(cxxn))
+  indx = c()
+  count = c()
+  rcov = list(tpairn = tpairn, 
+              cxxn = cxxn,
+              indx, indx, 
+              win = win,
+              cyy = cyy,
+              count = count)
+  if (method %in% c('GCV', 'GMeanAndGCV')){
+    gcvObj = GCVLwls2DV2(tgrid, tgrid, kern = kern, rcov = rcov, t = lapply(1:n, function(o) tgrid))
+    bwCov <- gcvObj$h
+    if (method == 'GMeanAndGCV') {
+      bwCov <- sqrt(bwCov * gcvObj$minBW)
+    } 
+  }
+  sC = Lwls2D(bwCov, "gauss", xin = cbind(rep(tgrid, times = t), rep(tgrid, each = t)), yin = as.vector(C), xout1 = tgrid, xout2 = tgrid)
+  
+  return(sC)
+}
