5614686c6947bf15aaa1cfb6850ff11c3946c55e
Yidong Zhou <ydzhou@ucdavis.edu>
2021-10-07 14:31:45 -0700

    frechet CPD

---
+++ R/NetCPDStatistic.R
@@ -0,0 +1,28 @@
+#' @description Helper function computing bootstrap replications of
+#'   the Fréchet CPD test statistics for networks.
+#' @noRd
+
+NetCPDStatistic <- function(data, indices, cutOff, bootSize = length(indices)) {
+  LyBoot <- data[indices[1:bootSize]] # booted sample. m see bootstrap scheme in the paper
+  n <- length(LyBoot) # number of observations
+  scope <- ceiling(cutOff * n):(n - ceiling(cutOff * n))
+  nTn <- sapply(scope, function(i) {
+    n1 <- i
+    n2 <- n - i
+    lambda <- n1 / n
+    LyBoot1 <- LyBoot[1:n1]
+    LyBoot2 <- LyBoot[(n1 + 1):n]
+    mup <- rowMeans(matrix(unlist(LyBoot), ncol = n)) # overall Frechet mean
+    mu1 <- rowMeans(matrix(unlist(LyBoot1), ncol = n1)) # first part Frechet mean
+    mu2 <- rowMeans(matrix(unlist(LyBoot2), ncol = n2)) # second part Frechet mean
+    V1 <- mean(sapply(LyBoot1, function(LyBoot1i) sum((LyBoot1i - mu1)^2)))
+    V2 <- mean(sapply(LyBoot2, function(LyBoot2i) sum((LyBoot2i - mu2)^2)))
+    V1C <- mean(sapply(LyBoot1, function(LyBoot1i) sum((LyBoot1i - mu2)^2)))
+    V2C <- mean(sapply(LyBoot2, function(LyBoot2i) sum((LyBoot2i - mu1)^2)))
+    Di <- sapply(LyBoot, function(LyBooti) sum((LyBooti - mup)^2))
+    sigma2 <- mean(Di^2) - mean(Di)^2
+    nTni <- n * lambda * (1 - lambda) * ((V1 - V2)^2 + (V1C - V1 + V2C - V2)^2) / sigma2 # formula 2.5 nTn
+    nTni
+  })
+  c(maxnTn = max(nTn), tau = which.max(nTn)) # test statistic and location of change point
+}
---
+++ tests/testthat/test_NetCPD.R
@@ -0,0 +1,133 @@
+library(testthat)
+# note: the following tests is very time-consuming 
+
+test_that("works for networks from the stochastic block model with different preferential matrices", {
+  set.seed(1)
+  n1 <- 30
+  n2 <- 30
+  C1 <- cbind(c(0.8, 0.3, 0.2), c(0.3, 0.6, 0.1), c(0.2, 0.1, 0.75)) # 3 groups in sbm
+  C2 <- matrix(c(0.9, 0.2, 0.2, 0.6), 2, 2) # 2 groups in sbm
+  Y1 <- lapply(1:n1, function(i) {
+    igraph::laplacian_matrix(igraph::sample_sbm(n = 20, pref.matrix = C1, 
+                                                block.sizes = c(5, 5, 10)), 
+                             sparse = FALSE)
+  })
+  Y2 <- lapply(1:n2, function(i) {
+    igraph::laplacian_matrix(igraph::sample_sbm(n = 20, pref.matrix = C2, 
+                                                block.sizes = c(10, 10)), 
+                             sparse = FALSE)
+  })
+  Ly <- c(Y1, Y2)
+  res <- NetCPD(Ly, optns = list(boot = TRUE))
+  expect_equal(res$pvalAsy < 1e-5 & res$pvalBoot < 1e-5, TRUE)
+})
+
+test_that("works for scale-free networks from the Barabasi-Albert model with different powers of the preferential attachment", {
+  set.seed(1)
+  n1 <- 100
+  n2 <- 100
+  gamma1 <- 2
+  gamma2 <- 3
+  Y1 <- lapply(1:n1, function(i) {
+    igraph::laplacian_matrix(igraph::sample_pa(n = 10, power = gamma1, 
+                                               directed = FALSE), 
+                             sparse = FALSE)
+  })
+  Y2 <- lapply(1:n2, function(i) {
+    igraph::laplacian_matrix(igraph::sample_pa(n = 10, power = gamma2, 
+                                               directed = FALSE), 
+                             sparse = FALSE)
+  })
+  Ly <- c(Y1, Y2)
+  res <- NetCPD(Ly, optns = list(boot = TRUE))
+  expect_equal(res$pvalAsy < 1e-3, TRUE)
+})
+
+test_that("works if the two populations are the same", {
+  set.seed(1)
+  n1 <- 100
+  n2 <- 100
+  C1 <- cbind(c(0.8, 0.3, 0.2), c(0.3, 0.6, 0.1), c(0.2, 0.1, 0.75)) # 3 groups in sbm
+  C2 <- C1
+  Y1 <- lapply(1:n1, function(i) {
+    igraph::laplacian_matrix(igraph::sample_sbm(n = 20, pref.matrix = C1, 
+                                                block.sizes = c(5, 5, 10)), 
+                             sparse = FALSE)
+  })
+  Y2 <- lapply(1:n2, function(i) {
+    igraph::laplacian_matrix(igraph::sample_sbm(n = 20, pref.matrix = C2, 
+                                                block.sizes = c(5, 5, 10)), 
+                             sparse = FALSE)
+  })
+  Ly <- c(Y1, Y2)
+  res <- NetCPD(Ly, optns = list(boot = TRUE))
+  expect_equal(res$pvalAsy > .05 & res$pvalBoot > .05, TRUE)
+})
+
+test_that("works for covariance matrices", {
+  set.seed(1)
+  n1 <- 100
+  n2 <- 100
+  U <- pracma::randortho(10)
+  Y1 <- lapply(1:n1, function(i) {
+    U %*% diag(rexp(10, (1:10)/2)) %*% t(U)
+  })
+  Y2 <- lapply(1:n2, function(i) {
+    U %*% diag(rexp(10, 1:10)) %*% t(U)
+  })
+  Ly <- c(Y1, Y2)
+  res <- NetCPD(Ly, optns = list(boot = TRUE))
+  expect_equal(res$pvalAsy < 1e-1 & res$pvalBoot < 1e-1, TRUE)
+})
+
+test_that("works for correlation matrices", {
+  set.seed(1)
+  n1 <- 100
+  n2 <- 100
+  m <- 10
+  d <- m * (m - 1) / 2
+  alpha1 <- 1
+  beta1 <- 1
+  alpha2 <- 2
+  beta2 <- 2
+  Y1 <- lapply(1:n1, function(i) {
+    yVec <- rbeta(d, shape1 = alpha1, shape2 = beta1)
+    y <- matrix(0, nrow = m, ncol = m)
+    y[lower.tri(y)] <- yVec
+    y <- y + t(y)
+    diag(y) <- 1
+    y
+  })
+  Y2 <- lapply(1:n2, function(i) {
+    yVec <- rbeta(d, shape1 = alpha2, shape2 = beta2)
+    y <- matrix(0, nrow = m, ncol = m)
+    y[lower.tri(y)] <- yVec
+    y <- y + t(y)
+    diag(y) <- 1
+    y
+  })
+  Ly <- c(Y1, Y2)
+  res <- NetCPD(Ly, optns = list(boot = TRUE))
+  expect_equal(res$pvalAsy < 1e-5 & res$pvalBoot < 1e-5, TRUE)
+})
+
+test_that("works for array input", {
+  set.seed(1)
+  n1 <- 30
+  n2 <- 30
+  C1 <- cbind(c(0.8, 0.3, 0.2), c(0.3, 0.6, 0.1), c(0.2, 0.1, 0.75)) # 3 groups in sbm
+  C2 <- matrix(c(0.9, 0.2, 0.2, 0.6), 2, 2) # 2 groups in sbm
+  Y1 <- lapply(1:n1, function(i) {
+    igraph::laplacian_matrix(igraph::sample_sbm(n = 20, pref.matrix = C1, 
+                                                block.sizes = c(5, 5, 10)), 
+                             sparse = FALSE)
+  })
+  Y2 <- lapply(1:n2, function(i) {
+    igraph::laplacian_matrix(igraph::sample_sbm(n = 20, pref.matrix = C2, 
+                                                block.sizes = c(10, 10)), 
+                             sparse = FALSE)
+  })
+  Ly <- array(unlist(c(Y1, Y2)), c(20, 20, n1 + n2))
+  res <- NetCPD(Ly, optns = list(boot = TRUE))
+  expect_equal(res$pvalAsy < 1e-5 & res$pvalBoot < 1e-5, TRUE)
+})
