4ea19cef34956ff819a05f383ec5a384ba583121
Kyunghee Han <hankh@uic.edu>
2023-02-28 22:33:07 -0600

    Renaming AddDensReg() instead of DensityFAM() on 2/28

--- R/AddDensReg.R
+++ R/AddDensReg.R
@@ -1,26 +1,33 @@
-#' Iterative Smooth Backfitting Algorithm
+#' @title Additive functional regression for densities as responses
 #'
-#' Smooth backfitting procedure for nonparametric functional additive models for density responses
+#' @description Smooth backfitting procedure for estimating nonparametric functional additive models for density responses
 #'
-#' @param Ly A list of \emph{n} vectors holding the observed random samples independently drawn from each density response
-#' @param X An \emph{n}-by-\emph{d} design matrix whose row vectors consist of regressors for each component function
-#' @param x An \emph{M}-by-\emph{d} matrix whose row vectors consist of evaluation points for each component function
-#' @param h0 A scalar bandwidth for kernel smoothing marginal mean function
-#' @param h A \emph{d}-dimensional vector bandwidths for kernel smoothing each component function
-#' @param densSup A \emph{2}-dimensional vector that specify the lower and upper limits of the common support for density responses. Defaults to the range of the observed random samples.
+#' @param Ly A list of \emph{n} vectors holding random samples independently drawn from each density response
+#' @param X An \emph{n}-by-\emph{d} design matrix whose row vectors consist of regressors for component functions
+#' @param x A grid matrix whose row vectors consist of evaluation points for component functions. Defaults to the observed design matrix.
+#' @param hu A scalar bandwidth for kernel smoothing marginal mean function of the functional additive model
+#' @param huA \emph{d}-dimensional vector bandwidth for kernel smoothing each component function
+#' @param dSup A \emph{2}-dimensional vector that specify the lower and upper limits of the common support for density responses. Defaults to the range of the observed random samples.
 #'
-#' @details \code{DensityFAM} fits component functions of functional additive models for density responses associated with multiple regressors (Han et al., 2020). 
-#' The current version only supports the log-quantile density (LQD) transformation approach (Petersen and Mueller, 2016). 
-#'
-#' @seealso SBFitting() in the \emph{fdapace} pacakge
+#' @details \code{AddDensReg} fits additive functional regression models for density responses, where the conditional mean function of a transformed density response is given by the summation of nonparametric univariate functions associated with \emph{d} covariates, respectively.
+#' Instead of having functional responses as infinite-dimensional random objects, \code{AddDensReg} has inputs with random samples independently drawn from each random density, and density responses are reconstructed by kernel density estimation.
+#' The current version only supports the log-quantile density (LQD) transformation proposed by Petersen and Mueller (2016).
 #'
 #' @return A list holding the following fields:
-#' \item{lqdGrid}{A grid where the LQD transformation of density responses is evaluated. Defaults to an equally space grid on [0,1].}
-#' \item{densGrid}{A grid where density responses are evaluated. Defaults to to an equally space grid on the range of the observed random samples.}
-#' \item{lqdSbfMean}{The estimates of the marginal mean function evaluated on \code{lqdGrid}}
-#' \item{LlqdSbfComp}{A list of \emph{d} matrices holding the estimates of each component functions evaluated on \code{lqdGrid}}
+#' \item{lqdGrid}{A grid where the LQD transformed density responses are evaluated. Defaults to an equally space grid over \code{dSup}.}
+#' \item{lqdSbfMean}{The marginal mean function estimates evaluated on \code{lqdGrid}}
+#' \item{LlqdSbfComp}{A list of \emph{d} matrices holding the estimates of each component function evaluated on \code{lqdGrid}}
+#' \item{densGrid}{A grid where density responses are evaluated. Defaults to an equally space grid over the range of the observed random samples.}
 #' \item{densSbfMean}{The density inversion of \code{lqdSbfMean} evaluated on \code{densGrid}}
-#' \item{LdensSbfComp}{A list of \emph{d} matrices holding the density inversion of \code{lqdSbfMean} together with each component function evaluated on \code{lqdGrid}}
+#' \item{LdensSbfComp}{A list of \emph{d} matrices holding the density inversion of each component function together with \code{lqdSbfMean} evaluated on \code{lqdGrid}}
+#'
+#' @references
+#' \cite{Han, K., Mueller, H.-G., and Park, B. U. (2020), "Additive functional regression for densities as responses", Journal of the American Statistical Association , 115 (530), pp.997-1010.}
+#'
+#' \cite{Peterson, A. and Mueller, H.-G. (2016), "Functional data analysis for density functions by transformation to a Hilbert space", The Annals of Statistics, 44(1), pp.183-218}
+#'
+#' @seealso \code{SBFitting} in the \code{fdapace} package
+#'
 #' @examples
 #' library(MASS)
 #' 
@@ -56,76 +63,67 @@
 #' }
 #' 
 #' M <- 51
-#' x0 <- seq(0, 1, length.out = M)
-#' x <- cbind(x0, x0)
+#' x1 <- x2 <- seq(0, 1, length.out = M)
+#' x <- cbind(x1, x2)
 #' 
-#' h0 <- 0.05
-#' h <- c(0.075, 0.075)
-#' densSup <- c(0, 1)
+#' hu <- 0.05
+#' hx <- c(0.075, 0.075)
+#' dSup <- c(0, 1)
 #' 
 #' 
 #' ### component function estimation
-#' # smooth backfitting
-#' estDensityFAM <- DensityFAM(Ly = Ly, X = X, x = x, h0 = h0, h = h, densSup = densSup)
-#' 
-#' # lqd evaluation grid
-#' lqdGrid <- estDensityFAM$lqdGrid
-#' lqdGridLen <- length(lqdGrid)
-#' 
-#' # density evaluation grid
-#' densGrid <- estDensityFAM$densGrid
-#' densGridLen <- length(densGrid)
+#' estAddDensReg <- AddDensReg(Ly = Ly, X = X, x = x, hu = hu, hx = hx, dSup = dSup)
 #' 
 #' # true LQD component functions
-#' g1Eval <- g2Eval <- matrix(nrow = lqdGridLen, ncol = M)
-#' for (l in 1:lqdGridLen) {
-#'   for (m in 1:M) {
-#'     g1Eval[l,m] <- g1(lqdGrid[l], x[m,1])
-#'     g2Eval[l,m] <- g2(lqdGrid[l], x[m,2])
+#' g1Eval <- g2Eval <- matrix(nrow = length(estAddDensReg$lqdGrid), ncol = M)
+#' for (l in seq(estAddDensReg$lqdGrid)) {
+#'   for (m in seq(M)) {
+#'     g1Eval[l,m] <- g1(estAddDensReg$lqdGrid[l], x1[m])
+#'     g2Eval[l,m] <- g2(estAddDensReg$lqdGrid[l], x2[m])
 #'   }
 #' }
 #' 
 #' # LQD component function estimates
-#' g0Sbf <- estDensityFAM$lqdSbfMean
-#' gjSbf <- estDensityFAM$LlqdSbfComp
+#' g0Sbf <- estAddDensReg$lqdSbfMean
+#' gjSbf <- estAddDensReg$LlqdSbfComp
 #' 
 #' # true density component functions
-#' dens1Eval <- dens2Eval <- matrix(nrow = densGridLen, ncol = M)
-#' for (m in 1:M) {
+#' dens1Eval <- dens2Eval <- matrix(nrow = length(estAddDensReg$densGrid), ncol = M)
+#' for (m in seq(M)) {
 #'   dens1Eval[,m] <- fdadensity::lqd2dens(lqd = g1Eval[,m],
-#'                                         lqSup = lqdGrid,
-#'                                         dSup = densGrid)
+#'                                         lqSup = estAddDensReg$lqdGrid,
+#'                                         dSup = estAddDensReg$densGrid)
 #'   dens2Eval[,m] <- fdadensity::lqd2dens(lqd = g2Eval[,m],
-#'                                         lqSup = lqdGrid,
-#'                                         dSup = densGrid)
+#'                                         lqSup = estAddDensReg$lqdGrid,
+#'                                         dSup = estAddDensReg$densGrid)
 #' }
 #' 
 #' # density component function estimates
-#' dens0Sbf <- estDensityFAM$densSbfMean
-#' densjSbf <- estDensityFAM$LdensSbfComp
+#' dens0Sbf <- estAddDensReg$densSbfMean
+#' densjSbf <- estAddDensReg$LdensSbfComp
 #' 
 #' # graphical illustration of LQD component function estimates
 #' par(mfrow = c(2,2))
 #' par(mar=rep(0.5, 4)+0.1)
-#' persp(lqdGrid, x[,1], g1Eval,
+#' persp(estAddDensReg$lqdGrid, x1, g1Eval,
 #'       theta = 35, phi = 35,
 #'       xlab = '\n u', ylab = '\n x1', zlab = '\n LQD component (g1)',
 #'       border = NA, shade = 0.5,
 #'       ticktype = 'detailed')
 #' 
-#' persp(lqdGrid, x[,2], g2Eval,
+#' persp(estAddDensReg$lqdGrid, x2, g2Eval,
 #'       theta = 35, phi = 35,
 #'       xlab = '\n u', ylab = '\n x1', zlab = '\n LQD component  (g2)',
 #'       border = NA, shade = 0.5,
 #'       ticktype = 'detailed')
 #' 
-#' persp(lqdGrid, x[,1], gjSbf[[1]],
+#' persp(estAddDensReg$lqdGrid, x1, gjSbf[[1]],
 #'       theta = 35, phi = 35,
 #'       xlab = '\n u', ylab = '\n x1', zlab = '\n SBF estimate (g1)',
 #'       border = NA, shade = 0.5,
 #'       ticktype = 'detailed')
 #' 
-#' persp(lqdGrid, x[,2], gjSbf[[2]],
+#' persp(estAddDensReg$lqdGrid, x2, gjSbf[[2]],
 #'       theta = 35, phi = 35,
 #'       xlab = '\n u', ylab = '\n x2', zlab = '\n SBF estimate (g2)',
 #'       border = NA, shade = 0.5,
@@ -134,25 +132,25 @@
 #' # graphical illustration of density component function estimates
 #' par(mfrow = c(2,2))
 #' par(mar=rep(0.5, 4)+0.1)
-#' persp(densGrid, x[,1], dens1Eval,
+#' persp(estAddDensReg$densGrid, x1, dens1Eval,
 #'       theta = 35, phi = 35,
 #'       xlab = '\n y', ylab = '\n x1', zlab = '\n\n Component density \n (Inversion of g0 + g1)',
 #'       border = NA, shade = 0.5,
 #'       ticktype = 'detailed')
 #' 
-#' persp(densGrid, x[,2], dens2Eval,
+#' persp(estAddDensReg$densGrid, x2, dens2Eval,
 #'       theta = 35, phi = 35,
 #'       xlab = '\n y', ylab = '\n x1', zlab = '\n\n Component density \n (Inversion of g0 + g2)',
 #'       border = NA, shade = 0.5,
 #'       ticktype = 'detailed')
 #' 
-#' persp(densGrid, x[,1], densjSbf[[1]],
+#' persp(estAddDensReg$densGrid, x1, densjSbf[[1]],
 #'       theta = 35, phi = 35,
 #'       xlab = '\n y', ylab = '\n x1', zlab = '\n\n SBF estimate \n (Inversion of g0 + g1)',
 #'       border = NA, shade = 0.5,
 #'       ticktype = 'detailed')
 #' 
-#' persp(densGrid, x[,2], densjSbf[[2]],
+#' persp(estAddDensReg$densGrid, x2, densjSbf[[2]],
 #'       theta = 35, phi = 35,
 #'       xlab = '\n y', ylab = '\n x1', zlab = '\n\n SBF estimate \n (Inversion of g0 + g2)',
 #'       border = NA, shade = 0.5,
@@ -160,28 +158,19 @@
 #' 
 #' 
 #' ### fitted density responses
-#' # smooth backfitting
-#' fitDensityFAM <- DensityFAM(Ly = Ly, X = X, h0 = h0, h = h, densSup = densSup)
-#' 
-#' # lqd evaluation grid
-#' lqdGrid <- fitDensityFAM$lqdGrid
-#' lqdGridLen <- length(lqdGrid)
-#' 
-#' # density evaluation grid
-#' densGrid <- fitDensityFAM$densGrid
-#' densGridLen <- length(densGrid)
+#' fitAddDensReg <- AddDensReg(Ly = Ly, X = X, hu = hu, hx = hx, dSup = dSup)
 #' 
 #' # fitted LQD component functions
-#' g0SBFit <- fitDensityFAM$lqdSbfMean
-#' gjSBFit <- fitDensityFAM$LlqdSbfComp
+#' g0SBFit <- fitAddDensReg$lqdSbfMean
+#' gjSBFit <- fitAddDensReg$LlqdSbfComp
 #' 
 #' # fitted density responses
 #' densSBFit <- lapply(1:n, 
 #'                     function (i) {
 #'                       gSBFit <- g0SBFit + gjSBFit[[1]][,i] + gjSBFit[[2]][,i]
 #'                       densSBFit_i <- fdadensity::lqd2dens(lqd = gSBFit, 
-#'                                                           lqSup = lqdGrid,
-#'                                                           dSup = densGrid)
+#'                                                           lqSup = fitAddDensReg$lqdGrid,
+#'                                                           dSup = fitAddDensReg$densGrid)
 #'                       return (densSBFit_i)
 #'                     }
 #' )
@@ -197,19 +186,12 @@
 #'        ylim = range(c(hist_i$density, densSBFit[[i]])),
 #'        xlab = 'Y',
 #'        main = paste(i, '-th random sample \n with X = (', round(X[i,1],2), ', ', round(X[i,2],2), ')', sep = ''))
-#'   lines(densGrid, densSBFit[[i]], col = 2, lwd = 2)
+#'   lines(fitAddDensReg$densGrid, densSBFit[[i]], col = 2, lwd = 2)
 #' }
 #' 
-#' @references
-#' \cite{Han, K., Mueller, H.-G., and Park, B. U. (2020), "Additive functional regression for densities as responses", Journal of the American Statistical Association , 115 (530), pp.997-1010.}
-#'
-#' \cite{Peterson, A. and Mueller, H.-G. (2019), "Frechet regression for random objects with Eucliean predictors", The Annals of Statistics, 47(2), pp.691-719.}
-#' 
-#' \cite{Peterson, A. and Mueller, H.-G. (2016), "Functional Data Analysis for Density Functions by Transformation to a Hilbert space", The Annals of Statistics, 44(1), pp.183-218}
-#'
 #' @export
 
-DensityFAM <- function (Ly, X, x = NULL, h0 = NULL, h = NULL, densSup = NULL) {
+AddDensReg <- function (Ly, X, x = NULL, hu = NULL, hx = NULL, dSup = NULL) {
   
   if (is.list(Ly) == FALSE) {
     
@@ -219,32 +201,33 @@ DensityFAM <- function (Ly, X, x = NULL, h0 = NULL, h = NULL, densSup = NULL) {
   
   if (is.null(ncol(X))) {
     
-    return (message('The design matrix must be multi-dimensional.'))
+    return (message('The design matrix must be multi-dimensional corresponding to additive component.'))
     
   }
   
   if (length(Ly) != nrow(X)) {
     
-    return (message('The number of subjects in Ly and X are different.'))
+    return (message('The sample sizes of responses and regressors are different.'))
     
   }
   
   if (is.null(x) == TRUE) {
     
-    message('The evaluation grid will be replaced by the design matrix.')
+    message('The evaluation grid will be replaced by the observed design matrix.')
     x <- X
 
   } else {
     
-    if (sum(apply(x, 2, diff) < 0) > 0) {
-      
-      message('The evaluation grid must be of increasing order. Sorted by increasing for each column.')
-      x <- apply(x, 2, sort)
-      
-    }
+    # if (sum(apply(x, 2, diff) < 0) > 0) {
+    #   
+    #   message('The evaluation grid must be in increasing order. Sorted by increasing for each column.')
+    #   x <- apply(x, 2, sort)
+    #   
+    # }
+    
     if (is.null(ncol(x))) {
       
-      return (message('The evaluation grid must be multi-dimensional corresponding to the support of each additive component.'))
+      return (message('The evaluation grid must be multi-dimensional corresponding to additive component.'))
       
     } else if (ncol(X) != ncol(x)) {
       
@@ -254,34 +237,34 @@ DensityFAM <- function (Ly, X, x = NULL, h0 = NULL, h = NULL, densSup = NULL) {
     
   }
   
-  if (is.null(h0)) {
+  if (is.null(hu)) {
     
-    h0 <- 0.05
+    hu <- 0.05
     
-  } else if (h0 <= 0 || h0 >= 1) {
+  } else if (hu <= 0 || hu >= 1) {
     
     return (message('The bandwidth for smoothing transformed densities should be chosen by a positive number less than 1.'))
     
   }
   
-  if (is.null(h)) {
+  if (is.null(hx)) {
     
-    h <- rep(0.25 * n^(-1/5), ncol(X)) * apply(apply(x, 2, range), 2, diff)
+    hx <- rep(0.25 * n^(-1/5), ncol(X)) * apply(apply(x, 2, range), 2, diff)
     
-  } else if (length(h) < 2) {
+  } else if (length(hx) < 2) {
     
     return (message('The bandwidth must be multi-dimensional.'))
     
-  } else if (min(h) <= 0 || max(h) >= 1) {
+  } else if (min(hx) <= 0 || max(hx) >= 1) {
     
     return (message('Bandwidths for additive component functions should be chosen by a positive number less than 1.'))
     
   }
   
-  if (is.null(densSup)) {
+  if (is.null(dSup)) {
    
     message('The density response is assumed to have the common support. The observed min and max will be used for the lower/upper limits of the support.') 
-    densSup <- range(unlist(Ly))
+    dSup <- range(unlist(Ly))
     
   }
   
@@ -291,10 +274,10 @@ DensityFAM <- function (Ly, X, x = NULL, h0 = NULL, h = NULL, densSup = NULL) {
   d <- ncol(X)
   
   # minimum bandwidth
-  hMin <- apply(apply(apply(X, 2, sort), 2, diff), 2, max)
+  hxMin <- apply(apply(apply(X, 2, sort), 2, diff), 2, max)
   for (j in 1:d) {
-    if (hMin[j]/2 > h[j]) {
-      h[j] <- hMin[j]/2
+    if (hx[j] < hxMin[j]) {
+      hx[j] <- hxMin[j]
     }
   }
   
@@ -307,7 +290,7 @@ DensityFAM <- function (Ly, X, x = NULL, h0 = NULL, h = NULL, densSup = NULL) {
   # normalize random samples to be supported on [0,1]
   normalizeLy <- lapply(1:n, 
                           function (i) {
-                            normalizeY_i <- (Ly[[i]] - densSup[1]) / diff(densSup)
+                            normalizeY_i <- (Ly[[i]] - dSup[1]) / diff(dSup)
                           }
                         )
   
@@ -347,7 +330,7 @@ DensityFAM <- function (Ly, X, x = NULL, h0 = NULL, h = NULL, densSup = NULL) {
   # smoothing LQD responses
   LlqdSmooth <- lapply(1:n, 
                         function (i) {
-                          lqdSmooth_i <- fdapace::Lwls1D(bw = h0,
+                          lqdSmooth_i <- fdapace::Lwls1D(bw = hu,
                                                          kernel_type = 'epan',
                                                          xin = lqdGrid,
                                                          yin = Llqd[[i]],
@@ -375,7 +358,7 @@ DensityFAM <- function (Ly, X, x = NULL, h0 = NULL, h = NULL, densSup = NULL) {
   
   for (l in 1:lqdGridLen) {
     
-    sbfSurf <- fdapace::SBFitting(lqdSmoothMat[,l], x, X, h = h)
+    sbfSurf <- fdapace::SBFitting(lqdSmoothMat[,l], x, X, h = hx)
     
     g0Sbf[l] <- sbfSurf$mY
     for (j in 1:d) {
@@ -404,7 +387,7 @@ DensityFAM <- function (Ly, X, x = NULL, h0 = NULL, h = NULL, densSup = NULL) {
                                         lqSup = lqdGrid,
                                         dSup = densGrid)
       
-      densjSbf[[j]][, m] <- fdapace::Lwls1D(bw = h0,
+      densjSbf[[j]][, m] <- fdapace::Lwls1D(bw = hu,
                                             kernel_type = 'epan',
                                             xin = densGrid,
                                             yin = densSbf_j,
@@ -415,11 +398,11 @@ DensityFAM <- function (Ly, X, x = NULL, h0 = NULL, h = NULL, densSup = NULL) {
   return (list(Ly = Ly,
                X = X,
                x = x,
-               h0 = h0,
-               h = h,
-               densSup = densSup,
+               hu = hu,
+               hx = hx,
+               dSup = dSup,
                lqdGrid = lqdGrid,
-               densGrid = (densGrid*diff(densSup) + densSup[1]),
+               densGrid = (dSup[1] + densGrid*diff(dSup)),
                lqdSbfMean = g0Sbf,
                LlqdSbfComp = gjSbf,
                densSbfMean = dens0Sbf,
